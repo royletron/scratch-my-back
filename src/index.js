2b4ffe844b238a35c9242532e306751828c67685
Darren <darren@diffblue.com>
2019-08-14 17:55:33 +0100

    Thinking about offscreen canvas

--- src/index.js
+++ src/index.js
@@ -8,15 +8,59 @@ const loadImage = (key, t) => {
   } else {
     imageCache[key] = new Image();
     imageCache[key].src = t;
+    imageCache[key].setAttribute("crossOrigin", "");
     return imageCache[key];
   }
 };
+
+const names = {
+  f: ["Sarah", "Lily", "Kiki", "Rosie"],
+  m: [
+    "Dave",
+    "Brian",
+    "Simon",
+    "Vlasto",
+    "James",
+    "Ed",
+    "Adam",
+    "Toby",
+    "Felix"
+  ]
+};
+
+const skins = [
+  "#ffe0bd",
+  "#ffcd94",
+  "#eac086",
+  "#ffad60",
+  "#ffe39f",
+  "#8d5524"
+];
+
+class Worker {
+  constructor() {
+    this.gender = Math.random() > 0.5 ? "m" : "f";
+    this.name =
+      names[this.gender][Math.floor(Math.random() * names[this.gender].length)];
+    this.skin = skins[Math.floor(Math.random() * skins.length)];
+    this.back = new OffscreenCanvas(16 * 10, 24 * 10);
+    this.ctx = this.back.getContext("2d");
+    let image = loadImage("back", back);
+    this.ctx.drawImage(image, 0, 0, image.width * 10, image.height * 10);
+    this.image = this.ctx.getImageData(0, 0, this.back.width, this.back.height);
+  }
+  draw(ctx) {
+    ctx.putImageData(this.image, 0, 0);
+  }
+}
 
 class Game {
   constructor(id) {
     this.running = true;
     this.canvas = document.getElementById(id);
     this.ctx = this.canvas.getContext("2d");
+    this.ctx.imageSmoothingEnabled = false;
+    this.members = [new Worker()];
     this.loop();
   }
   loop() {
@@ -41,7 +85,7 @@ class Game {
   draw() {
     this.ctx.fillStyle = "#0000ff";
     this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
-    this.ctx.drawImage(loadImage("back", back), 10, 10);
+    this.members.forEach(m => m.draw(this.ctx));
   }
   destroy() {
     this.running = false;
